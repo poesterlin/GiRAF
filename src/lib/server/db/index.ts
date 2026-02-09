82d06c050109ee5cfaf76b8ff35934705049e85b
Philip Oesterlin <philip.oesterlin@gmail.com>
2026-02-09 15:08:53 +0100

    simplify

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -2,24 +2,16 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import { building } from '$app/environment';
 import { env } from '$env/dynamic/private';
 import * as schema from './schema';
-
-import { SQL } from 'bun';
+import postgres from 'postgres';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
-if (!env.DATABASE_URL && !building) {
-    throw new Error('DATABASE_URL is not set');
-}
-
 function createDb() {
-    if (building) {
-        return drizzle.mock({ schema });
+    if (!env.DATABASE_URL && !building) {
+        throw new Error('DATABASE_URL is not set');
     }
-
-    const client = new SQL(env.DATABASE_URL!) as any;
-    client.options = {
-        parsers: {},
-        serializers: {}
-    };
+    
+    const client = postgres(env.DATABASE_URL);
+    drizzle(client, { schema });
 
     return drizzle({ client, logger: false, schema });
 }
